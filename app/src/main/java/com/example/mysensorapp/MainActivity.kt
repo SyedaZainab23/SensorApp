fbd736d52591926368e5208dd4a1e548b74cbd2a
syeda <syedazainab400@gmail.com>
2023-03-24 15:36:18 -0400

    db with random data

--- app/src/main/java/com/example/mysensorapp/MainActivity.kt
+++ app/src/main/java/com/example/mysensorapp/MainActivity.kt
@@ -18,13 +18,32 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
     private lateinit var accelerometerSensor: Sensor
     private var isWalking = false
+    private var state = "Walking"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+            // below we have created
+            // a new DBHelper class,
+            // and passed context to it
+        val db = DBHelper(this, null)
+
+            // creating variables for values
+            // in name and age edit texts
+        val name = "enterName.text.toString()"
+        val age = "1234567890"
+        val activity = state
+            // calling method to add
+            // name to our database
+        db.addName(name, age, activity)
+
+            // Toast to message on the screen
+        Toast.makeText(this, name + " added to database", Toast.LENGTH_LONG).show()
+
+
     }
 
     override fun onResume() {
@@ -49,10 +68,12 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 val acceleration = sqrt(it.values[0] * it.values[0] + it.values[1] * it.values[1] + it.values[2] * it.values[2])
                 if (acceleration > 11) {
                     isWalking = true
+                    state="Walking"
                     showToast(this, "WALKING!")
                     // Do something when walking is detected
                 } else if (acceleration < 11) {
                     isWalking = false
+                    state="Still"
                     showToast(this, "STILL!")
                     // Do something when stillness is detected
                 }
